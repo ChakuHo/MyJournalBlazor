5eed102ad3eda0cd64379b098d93f4af1dee6ad5
BigyanHo <bigyan12341@gmail.com>
2025-12-13 17:59:33 +0545

    Add project files.

---
+++ MyJournalBlazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyJournalBlazor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyJournalBlazor/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyJournalBlazor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
